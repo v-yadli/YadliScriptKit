e10818e054cece3d69022fbc24a756c75b5ba160
Yatao Li <yatao.li@live.com>
2016-07-17 00:07:45 +0800

    v11

--- system/etc/init.qcom.post_boot.sh
+++ system/etc/init.qcom.post_boot.sh
@@ -24,17 +24,16 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# {{{ Prolog - hotplug, etc.
 #
 # enable adaptive LMK
 # Yadli: TODO: adaptive LMK not supported by our kernel.
 # echo 81250 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
 # echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
 
+
 target=`getprop ro.board.platform`
-# ensure at most one A57 is online when thermal hotplug is disabled
-echo 0 > /sys/devices/system/cpu/cpu5/online
-echo 0 > /sys/devices/system/cpu/cpu6/online
-echo 0 > /sys/devices/system/cpu/cpu7/online
 # Limit A57 max freq from msm_perf module in case CPU 4 is offline
 echo "4:960000 5:960000 6:960000 7:960000" > /sys/module/msm_performance/parameters/cpu_max_freq
 # disable thermal bcl hotplug to switch governor
@@ -73,36 +72,90 @@ done
 echo 25 > /sys/class/devfreq/qcom,cpubw.31/bw_hwmon/io_percent
 echo 5 > /sys/class/kgsl/kgsl-3d0/default_pwrlevel
 echo 50 > /sys/class/kgsl/kgsl-3d0/idle_timer
+#}}}
 
-# configure governor settings for little cluster
-echo "interactive" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/use_migration_notif
-echo 16000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/above_hispeed_delay
-echo 105 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/timer_rate
-echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/hispeed_freq
-echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/io_is_busy
-echo "95 960000:90 1340000:75" > /sys/devices/system/cpu/cpu0/cpufreq/interactive/target_loads
-echo 40000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/min_sample_time
-#echo 80000 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/max_freq_hysteresis
-echo 384000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-#echo 960000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
-# online CPU4
-echo 1 > /sys/devices/system/cpu/cpu4/online
-# configure governor settings for big cluster
-echo "interactive" > /sys/devices/system/cpu/cpu4/cpufreq/scaling_governor
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_sched_load
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/use_migration_notif
-echo "19000 1400000:39000 1700000:19000" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/above_hispeed_delay
-echo 105 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/go_hispeed_load
-echo 20000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/timer_rate
-echo 1248000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/hispeed_freq
-echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/io_is_busy
-echo "85 1500000:90 1800000:70" > /sys/devices/system/cpu/cpu4/cpufreq/interactive/target_loads
-echo 40000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/min_sample_time
-#echo 80000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
-echo 384000 > /sys/devices/system/cpu/cpu4/cpufreq/scaling_min_freq
+# {{{Yadli: cpufreq: settings initial parameters and bounds
+
+# LITTLE
+for i in {0,1,2,3}
+do
+    chmod 644 /sys/devices/system/cpu/cpu$i/online
+    echo 1 > /sys/devices/system/cpu/cpu$i/online
+    chmod 444 /sys/devices/system/cpu/cpu$i/online
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    echo interactive > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    chmod 444 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    echo 384000 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    chmod 444 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+    echo 1555200 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+done
+# big
+for i in {4,5,6,7}
+do
+    chmod 644 /sys/devices/system/cpu/cpu$i/online
+    echo 1 > /sys/devices/system/cpu/cpu$i/online
+    chmod 444 /sys/devices/system/cpu/cpu$i/online
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    echo interactive > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    chmod 444 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_governor
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    echo 384000 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    chmod 444 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_min_freq
+    chmod 644 /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+    echo 1958400 > /sys/devices/system/cpu/cpu$i/cpufreq/scaling_max_freq
+done
+
+#}}}
+
+# Yadli: at this point all 8 cores are operational. Setup governors
+# before inserting core_ctl.
+
+# LITTLE parameters, taken from DarkSpice on XperiaZ5
+for i in {0,1,2,3}
+do
+    echo 200 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/go_hispeed_load
+    echo 60000 768000:50000 960000:30000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/above_hispeed_delay 
+    echo 50000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/timer_rate 
+    echo 384000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/hispeed_freq
+    echo -1 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/timer_slack
+    # Yadli: touch low freq target loads, to ease screen-off situation.
+    echo 95 384000:75 460000:60 600000:75 672000:14 768000:80 864000:11 960000:98 1248000:8 1344000:99 1478000:100 1555200:100 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/target_loads
+    echo 85000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/min_sample_time
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/boost
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/align_windows
+    echo 1 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/use_migration_notif
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/use_sched_load
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/max_freq_hysteresis
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/boostpulse_duration
+done
+
+# big
+for i in {4,5,6,7}
+do
+    echo 200 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/go_hispeed_load
+    echo "60000 1344000:50000 1632000:30000" > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/above_hispeed_delay
+    echo 10000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/timer_rate
+    echo 384000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/hispeed_freq
+    echo 40000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/timer_slack
+    echo "97 480000:95 633000:80 768000:75 864000:65 960000:99 1248000:95 1344000:96 1440000:97 1536000:98 1632000:99 1728000:100 1824000:100" > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/target_loads
+    echo 10000 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/min_sample_time
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/boost
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/align_windows
+    echo 1 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/use_migration_notif
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/use_sched_load
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/max_freq_hysteresis
+    echo 0 > /sys/devices/system/cpu/cpu$i/cpufreq/interactive/boostpulse_duration
+done
+
+# input boost configuration
+
+echo 0:960000 1:960000 2:960000 3:960000 4:0 5:0 6:0 7:0 > /sys/module/cpu_boost/parameters/input_boost_freq
+echo 0 > /sys/module/cpu_boost/parameters/boost_ms
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms
+
+#{{{ Yadli: core_ctl module
 # insert core_ctl module and use conservative paremeters
 insmod /system/lib/modules/core_ctl.ko
 echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
@@ -120,31 +173,32 @@ for mode in /sys/devices/soc.0/qcom,bcl.*/mode
 do
     echo -n enable > $mode
 done
+
 # enable LPM
 echo 0 > /sys/module/lpm_levels/parameters/sleep_disabled
 # Restore CPU 4 max freq from msm_performance
 echo "4:4294967295 5:4294967295 6:4294967295 7:4294967295" > /sys/module/msm_performance/parameters/cpu_max_freq
-# input boost configuration
-echo 0:1248000 > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 30 > /sys/module/cpu_boost/parameters/input_boost_ms
-# core_ctl module
+
 # Yadli: Little cluster
-echo 2 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus
+echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/max_cpus
 echo 1 > /sys/devices/system/cpu/cpu0/core_ctl/min_cpus
 echo 80 > /sys/devices/system/cpu/cpu0/core_ctl/busy_up_thres
 echo 50 > /sys/devices/system/cpu/cpu0/core_ctl/busy_down_thres
-echo 60 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
+echo 100 > /sys/devices/system/cpu/cpu0/core_ctl/offline_delay_ms
 echo 0 > /sys/devices/system/cpu/cpu0/core_ctl/is_big_cluster
 echo 4 > /sys/devices/system/cpu/cpu0/core_ctl/task_thres
 # Yadli: big cluster
 echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/max_cpus
 echo 0 > /sys/devices/system/cpu/cpu4/core_ctl/min_cpus
-echo 80 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
+echo 90 > /sys/devices/system/cpu/cpu4/core_ctl/busy_up_thres
 echo 50 > /sys/devices/system/cpu/cpu4/core_ctl/busy_down_thres
-echo 150 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
+echo 300 > /sys/devices/system/cpu/cpu4/core_ctl/offline_delay_ms
 echo 1 > /sys/devices/system/cpu/cpu4/core_ctl/is_big_cluster
 echo 4 > /sys/devices/system/cpu/cpu4/core_ctl/task_thres
-# Setting b.L scheduler parameters
+#}}}
+
+
+# {{{Yadli: Setting b.L scheduler parameters
 echo 1 > /proc/sys/kernel/power_aware_timer_migration
 echo 1 > /proc/sys/kernel/sched_migration_fixup
 echo 30 > /proc/sys/kernel/sched_small_task
@@ -183,6 +237,7 @@ for devfreq_gov in /sys/class/devfreq/qcom,mincpubw*/governor
 do
     echo "cpufreq" > $devfreq_gov
 done
+#}}}
 
 #Yadli: GPU governor setting
 # echo simple_ondemand > /sys/class/kgsl/kgsl-3d0/devfreq/governor
@@ -225,8 +280,35 @@ if [ -c /dev/coresight-stm ]; then
     fi
 fi
 
-#Yadli: reinforce selinux policy
-#setenforce 0
+setenforce 0
 
 # Start RIDL/LogKit II client
 su -c /system/vendor/bin/startRIDL.sh &
+
+# {{{Yadli: setprop tweaks
+#
+#Battery Tweaks
+
+setprop pm.sleep_mode 1
+setprop ro.ril.disable.power.collapse 0
+setprop ro.ril.power_collapse 1
+setprop ro.config.hw_power_saving 1
+setprop ro.ril.fast.dormancy.rule 0
+setprop ro.config.hw_fast_dormancy 1
+setprop power_supply.wakeup enable
+setprop power.saving.mode 1
+setprop ro.config.hw_quickpoweron true
+
+#Performance Tweaks
+
+setprop ro.kernel.android.checkjni 0
+setprop ro.kernel.checkjni 0
+setprop debug.performance.tuning 1
+setprop debug.enabletr true
+setprop debug.overlayui.enable 1
+setprop dalvik.vm.checkjni false
+setprop dalvik.vm.verify-bytecode false
+setprop dalvik.vm.jmiopts forcecopy
+setprop dalvik.vm.dexopt-data-only 1
+
+# }}}
